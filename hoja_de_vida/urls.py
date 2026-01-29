acb1ab5ae1ae0769b7fa30812d617294c8d92ab0
gael07 <delgadoalvarez544@gmail.com>
2026-01-28 21:01:16 -0500

    Corrigiendo typo server por serve en urls

--- hoja_de_vida/urls.py
+++ hoja_de_vida/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings  # <--- AÑADE ESTO
 from django.conf.urls.static import static # <--- AÑADE ESTO
-
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('curriculum.urls')),
@@ -13,4 +13,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', server, {'document_root': settings.MEDIA_ROOT})]
+# Cambia "server" por "serve"
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
