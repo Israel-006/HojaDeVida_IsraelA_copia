81cc5015d7477ef6d8c8013c31996244d845f34c
gael07 <delgadoalvarez544@gmail.com>
2026-01-28 22:57:21 -0500

    Fix: desactivar compresion whitenoise

--- build.sh
+++ build.sh
@@ -18,4 +18,4 @@ python manage.py migrate
 
 # Crear superusuario automáticamente
 # Cambia 'admin' y 'mi_password_segura' por lo que tú quieras
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('gael07', 'delgadoalvarez544@gmail.com', '123456789') if not User.objects.filter(username='gael07').exists() else print('El usuario ya existe')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('gael06', 'delgadoalvarez544@gmail.com', '123456789') if not User.objects.filter(username='gael06').exists() else print('El usuario ya existe')" | python manage.py shell
